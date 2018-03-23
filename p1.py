4150f95fd013b80ddaed7801c2e243978e9c568d
Fred Guth <fredguth@fredguth.com>
2018-03-23 13:05:19 -0300

    closes requisito 2

--- p1.py
+++ p1.py
@@ -4,15 +4,17 @@ import cv2
 
 global img
 # img = cv2.imread('dummy.png',cv2.IMREAD_UNCHANGED)
-img = cv2.imread('venn.png',cv2.IMREAD_COLOR)
+img = cv2.imread('Lenna.jpg',cv2.IMREAD_COLOR)
 imgType = type(img[0,0]).__name__
 
 def dist(img, point):
+    # TODO: test np.hypot
     lines, columns, channels = img.shape
     A = img.reshape((lines*columns, channels))
     B = np.zeros(shape=(1,3))
     B[0]=point
     distances = sp.distance.cdist(A,B, metric='euclidean')
+    distances = distances.reshape((lines, columns, 1))
     return distances
      
 
@@ -28,40 +30,12 @@ def mouse_callback(event, column, line, flags, params):
             lines, columns, channels = img.shape
             result_image = np.zeros((lines, columns, channels), np.uint8)
             result_image[:,:]=(0,0,255)
-            
-            
-
-            # result_image = cv2.bitwise_and(result_image, (mask * img))
-            # cv2.imshow('result1', mask*img)
-            # cv2.imshow('result2', inv_mask*result_image)
             red = cv2.add((mask*img), (inv_mask*result_image))
             cv2.imshow('red', red)
+            cv2.moveWindow('red', columns, 45)
 def create_mask (img, point):
-    distances = dist(img, point)
-    lines, columns, channels = img.shape
-    mask = distances.reshape((lines, columns, 1))
-    mask = (mask>=13)
-    return mask
-def apply_mask (img, mask): 
+    return (dist(img, point)>=13)
 
-    # I want to put mask on top-left corner, So I create a ROI
-    rows,cols,channels = img.shape
-    roi = img[0:rows, 0:cols ]
-    
-    # # Now create a mask of logo and create its inverse mask also
-    # img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
-    # print (img2gray)
-    ret, bgmask = cv2.threshold(mask, 10, 255, cv2.THRESH_BINARY)
-    cv2.imgshow('bg', bgmask)
-    # mask_inv = cv2.bitwise_not(mask)
-    # # Now black-out the area of logo in ROI
-    # img1_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
-    # # Take only region of logo from logo image.
-    # img2_fg = cv2.bitwise_and(img2,img2,mask = mask)
-    # # Put logo in ROI and modify the main image
-    # dst = cv2.add(img1_bg,img2_fg)
-    # img1[0:rows, 0:cols ] = dst
-    # cv2.imgwrite('result.png', img1)
 
 while(1):
     cv2.imshow('window',img)
