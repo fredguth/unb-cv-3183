8b3e18b95e01c22732a03436a01b870db2adc589
Fred Guth <fredguth@fredguth.com>
2018-04-12 00:52:46 -0300

    began converting given code to python

--- p2/requisito2.py
+++ p2/requisito2.py
@@ -2,7 +2,12 @@ import numpy as np
 import cv2
 
 
-img = cv2.imread('./media/baboon.jpg', cv2.IMREAD_COLOR)
+video = cv2.VideoCapture(0)
+# set display at 640x360
+video.set(3, 640)
+video.set(4, 360)
+
+
 data = {
     "isMeasuring": False,
     "p1":[],
@@ -28,10 +33,22 @@ def mouse_callback(event, column, line, flags, params):
             dist = np.linalg.norm(p2-p1)
             print("dist:{}".format(dist))
 
+def snapshot(image):
+    print (image)
+
 while(1):
-    cv2.imshow('window',img)
-    cv2.setMouseCallback('window',mouse_callback, data)
-    k = cv2.waitKey(33)
-    if k==27:    # Esc key to stop
+    ret, frame = video.read()
+
+    if ret==True:
+        cv2.imshow('stream',frame)
+        # cv2.setMouseCallback('stream',mouse_callback, data)
+        k = cv2.waitKey(2) & 0xFF
+        if k==27:    # Esc key to stop
+            break
+        if k==32:    # Espace bar to snapshot
+            snapshot(frame)
+    else:
         break
 
+video.release()
+cv2.destroyAllWindows()
