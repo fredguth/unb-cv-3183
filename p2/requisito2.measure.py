a7b7b84d0eda3720d6fe97873372d7494b534c9d
Fred Guth <fredguth@fredguth.com>
2018-04-13 22:42:24 -0300

    requisito2 done

--- p2/requisito2.measure.py
+++ p2/requisito2.measure.py
@@ -4,7 +4,7 @@ import cv2
 intrinsic = np.load('Intrinsics.npy')
 distCoeff = np.load('Distortion.npy')
 capture = cv2.VideoCapture(0)
-# capture.set(cv2.CAP_PROP_FPS, 15)
+capture.set(cv2.CAP_PROP_FPS, 15)
 capture.set(3, 640)
 capture.set(4, 360)
 cv2.namedWindow("Raw")
@@ -35,8 +35,19 @@ def mouse_callback(event, column, line, flags, params):
             p2 = np.asarray([column, line])
             params["p2"] = p2
             
-            
 
+def drawLine (img, data, color):
+    p1 = data["p1"]
+    p2 = data["p2"]
+    if (p2[0] > 0):
+        cv2.line(img,tuple(p1),tuple(p2),color,2)
+        print ("p1, p2: {}, {}".format(p1, p2))
+        dist = np.linalg.norm(p2-p1)
+        h, w, c = img.shape
+        cv2.putText(img,"{} pixels".format(dist),(10,h-20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,color,1,cv2.LINE_AA)
+        print("dist:{}".format(dist))           
+    return img
+    
 while(capture.isOpened()):
     _, image = capture.read()    
     image = cv2.flip( image, 1)  # mirrors image
@@ -52,29 +63,12 @@ while(capture.isOpened()):
     cv2.setMouseCallback('Raw',mouse_callback, raw)
     cv2.setMouseCallback('Undistorted',mouse_callback, undistorted)
 
-    # TODO: move to function DRY
-    p1 = raw["p1"]
-    p2 = raw["p2"]
-    if (p2[0] > 0):
-        cv2.line(image,tuple(p1),tuple(p2),(33,255,33),2)
-        print ("p1, p2: {}, {}".format(p1, p2))
-        dist = np.linalg.norm(p2-p1)
-        h, w, c = image.shape
-        cv2.putText(image,"{} pixels".format(dist),(10,h-20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(33,255,33),1,cv2.LINE_AA)
-        print("dist:{}".format(dist))
+    
+    image = drawLine(image, raw, (33,255,33))
     cv2.imshow('Raw', image)
     
-
-    # p = undistorted["p1"]
-    # p2 = undistorted["p2"]
-    # if (p2[0] > 0):
-    #     cv2.line(dst,tuple(p1),tuple(p2),(33,255,33),2)
-    #     print ("p1, p2: {}, {}".format(p1, p2))
-    #     dist = np.linalg.norm(p2-p1)
-    #     h, w, c = dst.shape
-    #     cv2.putText(dst,"{} pixels".format(dist),(10,h-20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(33,255,33),1,cv2.LINE_AA)
-    #     print("dist:{}".format(dist))
-    # cv2.imshow('Undistorted', dst)
+    dst = drawLine(dst, undistorted, (255,33,255))
+    cv2.imshow('Undistorted', dst)
     
     k = cv2.waitKey(60) & 0xFF
     if k==27:    # Esc key to stop
