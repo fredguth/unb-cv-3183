aebd0a9e97c4f722c17356a44847168834fe2f35
Fred Guth <fredguth@fredguth.com>
2018-06-28 18:59:24 -0300

    code refactoring

--- pfinal/GTmaker.py
+++ pfinal/GTmaker.py
@@ -10,33 +10,19 @@ from sklearn.metrics import pairwise_distances_argmin
 from sklearn.datasets import load_sample_image
 from sklearn.utils import shuffle
 from time import time
+from sklearn.externals import joblib
 
-def findClusters (images):
+def createClassifier (images):
   n_colors = 64
-
-  # # Convert to floats instead of the default 8 bits integer coding. Dividing by
-  # # 255 is important so that plt.imshow behaves works well on float data (need to
-  # # be in the range [0-1])
-  # images = np.array(images, dtype=np.float64) / 255
-
   n, h, w, d = original_shape = tuple(images.shape)
   assert d == 3
   image_array = np.reshape(images, ( n*h*w, d))
 
   print("Fitting model on a small sub-sample of the data")
   image_array_sample = shuffle(image_array, random_state=0)[:n*1000]
-  kmeans = KMeans(n_clusters=n_colors, random_state=0).fit(image_array_sample)
+  clf = KMeans(n_clusters=n_colors, random_state=0).fit(image_array_sample)
   print ("Model fit complete")
-  return kmeans
-
-def getAllColors ():
-  table = []
-  for B in range(256):
-    for G in range (256):
-      for R in range (256):
-        table.append((B,G,R))
-  table = np.asarray(table)
-  return table
+  return clf
 
 def getGTData():
   # Xgt = original source data of groundtruth
@@ -45,9 +31,9 @@ def getGTData():
   Xgt = []
   G = []
   for filename in GTfiles:
+    print ('r', filename)
     xgFile = filename.replace('GT', 'dataset')
     xgFile = xgFile.replace('-mask', '')
-    print ('r', xgFile)
     gtImage = cv2.imread(filename)
     xgImage = cv2.imread(xgFile)
     xgImage = cv2.resize(xgImage, (448,448))
@@ -57,66 +43,57 @@ def getGTData():
   G = np.asarray(G)
   return Xgt, G
 
-def makeMaps(X):
-  allColors = getAllColors()
-  kmeans = findClusters(X)
-  labels = kmeans.predict(allColors)
-  cc = kmeans.cluster_centers_
-  
-  colormap = allColors
+# LUT is a look up table that says for each 64 colors, which are most 
+# likely foreground and which are most likely background
+def createLUT(Xgt, G):
+  n, h, w, d = original_shape = tuple(Xgt.shape)
+  Xgt= np.reshape(Xgt, ( n*h*w, d))
+  labels = clf.predict(Xgt)
+  gt = G/255
+  gt = gt[:,:,:,0]
+  gn, hn, wn= gt.shape
+  gt = np.reshape(gt,(gn*hn*wn,))
+
+  #foreground == 1
+  #background == 0
+  lut = np.zeros((64, 2))
   for i in range(len(labels)):
-    colormap[i] = cc[labels[i]]
-  colormap = np.reshape(colormap, (256, 256, 256, 3))
-  labelmap = np.reshape(labels, (256, 256, 256))
-  return labelmap, colormap
+    label = labels[i]
+    if gt[i]==0: # background
+      lut[label, 0] +=1
+    else: # foreground
+      lut[label, 1] += 1
+
+  lut =  (lut[:,1]>lut[:,0]).astype("uint8")
+  return lut
 
 #__main__
 Xgt, G = getGTData()
-kmeans = findClusters(Xgt)
+clf = createClassifier(Xgt)
+joblib.dump(clf, 'quantitizer.pkl') 
+lut = createLUT(Xgt, G)
 
-n, h, w, d = original_shape = tuple(Xgt.shape)
-Xgt= np.reshape(Xgt, ( n*h*w, d))
-labels = kmeans.predict(Xgt)
-# y = np.zeros(Xgt.shape)
-# y = kmeans.cluster_centers_[labels]
-
-gt = G/255
-gt = gt[:,:,:,0]
-gn, hn, wn= gt.shape
-gt = np.reshape(gt,(gn*hn*wn,))
-
-
-#foreground == 1
-#background == 0
-lut = np.zeros((64, 2))
-for i in range(len(labels)):
-  label = labels[i]
-  if gt[i]==0: # background
-    lut[label, 0] +=1
-  else: # foreground
-    lut[label, 1] += 1
-
-lut =  (lut[:,1]>lut[:,0]).astype("uint8")
-# print (lut, lut.sum())
 
 filenames = glob.glob('./dataset/*/*')
 
-for filename in filenames[3:4]:
-  print(filename)
+for filename in filenames:
+  
   x = cv2.imread(filename)
   x = cv2.resize(x, (448,448))
   (xh, xw, xd) = xshape = tuple(x.shape)
   x = np.reshape(x, (xh*xw, xd))
-  xl = kmeans.predict(x)
+  xl = clf.predict(x)
   y = np.zeros(x.shape)
-  print (kmeans.cluster_centers_.shape, lut.shape)
   y = lut[xl]*255
   y = np.reshape(y, (xh, xw))
   
-  filename = filename.replace('dataset', 'GT')
-  filename = filename.replace('.jpg', '-mmask.jpg')
+  filename = filename.replace('dataset', 'mask')
+  filename = filename.replace('.jpg', '-mask.jpg')
+  directory = filename.split('/')
+  directory = '/'.join(directory[:-1])
+  if not os.path.exists(directory):    
+      os.makedirs(directory)
   print ('w', filename)
   cv2.imwrite(filename, y)
 
-# Xlabels = kmeans.predict(X)
 
